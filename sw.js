9494d7eca9ec40040b022e86ad90ebd0aa5ea296
이성헌 <a39161026@gmail.com>
2026-09-14 09:31:26 +0900

    fix statColor

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'baseball9-v3';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./baseball-manager.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('generativelanguage.googleapis.com')) return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./baseball-manager.html'))));
+});
